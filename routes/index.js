eafbbe3e17c452bbed8efb1bd4375fd6cf7dea31
adriennecordell <adrienne.cordellac@gmail.com>
2023-02-21 13:14:40 -0600

    updated import

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+import express from "express";
+const router = express.Router();
+import apiRoutes from "./api";
 
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+	res.send("<h1>Wrong Route!</h1>");
 });
 
-module.exports = router;
+export default router;
